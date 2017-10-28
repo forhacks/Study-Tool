7961f9fae2888668e691e902bf9f3563a89bee79
Oliver Ni <oliver.ni@gmail.com>
2017-10-28 06:12:56 -0700

    Fix frontend
    Add lots of stuff

--- study/urls.py
+++ study/urls.py
@@ -7,12 +7,14 @@ app_name = 'study'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^login/$', views.LoginView.as_view(), name='login'),
+    url(r'^signup/$', views.SignUpView.as_view(), name='signup'),
     url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
     url(r'^dashboard/', include([
         url(r'^$', views.DashboardView.as_view(), name='index'),
-        url(r'^new/$', views.NewDeckView.as_view(), name='new'),
+        url(r'^new/$', views.CreateDeckView.as_view(), name='new'),
         url(r'^deck/(?P<pk>[0-9]+)/', include([
-            url(r'^$', views.DeckView.as_view(), name='index')
+            url(r'^$', views.DeckView.as_view(), name='index'),
+            url(r'^edit$', views.update_deck_view, name='edit')
         ], namespace='deck'))
     ], namespace='dashboard'))
 ]
--- study/views.py
+++ study/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.views import generic
 from django.urls import reverse
 
-from .models import Deck
+from .models import Deck, Card
 from .forms import DeckForm
 
 
@@ -17,6 +19,21 @@ class LoginView(generic.FormView):
     form_class = AuthenticationForm
 
     def form_valid(self, form):
+        data = form.cleaned_data
+        user = authenticate(form.request, username=data['username'], password=data['password'])
+
+        if user is not None:
+            login(self.request, user)
+            return HttpResponseRedirect(reverse('study:dashboard:index'))
+
+
+class SignUpView(generic.FormView):
+    template_name = 'study/signup.html'
+    form_class = UserCreationForm
+
+    def form_valid(self, form):
+        form.save()
+
         data = form.cleaned_data
         user = authenticate(form.request, username=data['username'], password=data['password'])
 
@@ -40,7 +57,7 @@ class DashboardView(generic.TemplateView):
         return context
 
 
-class NewDeckView(generic.FormView):
+class CreateDeckView(generic.CreateView):
     template_name = 'study/new_deck.html'
     form_class = DeckForm
 
@@ -48,10 +65,10 @@ class NewDeckView(generic.FormView):
         deck = form.save(commit=False)
         deck.owner = self.request.user
         deck.save()
-        return HttpResponseRedirect(reverse('study:dashboard:index'))
+        return HttpResponseRedirect(reverse('study:dashboard:deck:index', args=(deck.id,)))
 
     def get_context_data(self, **kwargs):
-        context = super(NewDeckView, self).get_context_data(**kwargs)
+        context = super(CreateDeckView, self).get_context_data(**kwargs)
         context['user_decks'] = Deck.objects.filter(owner=self.request.user)
         return context
 
@@ -64,3 +81,41 @@ class DeckView(generic.DetailView):
         context = super(DeckView, self).get_context_data(**kwargs)
         context['user_decks'] = Deck.objects.filter(owner=self.request.user)
         return context
+
+
+class UpdateDeckView(generic.UpdateView):
+    template_name = 'study/edit_deck.html'
+    form_class = DeckForm
+    model = Deck
+
+    def form_valid(self, form):
+        deck = form.save()
+        return HttpResponseRedirect(reverse('study:dashboard:deck:index', args=(deck.id,)))
+
+    def get_context_data(self, **kwargs):
+        context = super(UpdateDeckView, self).get_context_data(**kwargs)
+        context['user_decks'] = Deck.objects.filter(owner=self.request.user)
+        return context
+
+
+def update_deck_view(request, pk):
+    deck = get_object_or_404(Deck, pk=pk)
+
+    CardInlineFormSet = inlineformset_factory(Deck, Card, fields=('term', 'definition'))
+
+    if request.method == "POST":
+        formset = CardInlineFormSet(request.POST, instance=deck)
+        if formset.is_valid():
+            formset.save()
+            return HttpResponseRedirect(reverse('study:dashboard:deck:index', args=(deck.id,)))
+    else:
+        formset = CardInlineFormSet(instance=deck)
+        print('test')
+
+    context = {
+        'formset': formset,
+        'deck': deck,
+        'user_decks': Deck.objects.filter(owner=request.user)
+    }
+
+    return render(request, 'study/edit_deck.html', context)
